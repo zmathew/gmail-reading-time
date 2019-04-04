ed8ae7ab8cb96ecf0f3114a9c702e3d2561af021
Zach Mathew <zmathew.to@gmail.com>
2019-04-04 12:24:46 -0400

    Adds contents to repo

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# gmail-reading-time
+# Email Reading Time Gmail Chrome Extension
 Chrome extension for Gmail to display the reading time of your email while composing it.
+
+![Screenshot of email reading time displayed in Gmail](demo/gmail-example.png?raw=true "Email Reading Time Gmail Chrome Extension")
+
+
+## Installation
+
+* [Download this repo as a zip](archive/master.zip).
+* Open [Chrome Extensions Tab](chrome://extensions).
+* Enable **Developer mode** (top right).
+* Drag the downloaded zip into the browser window.
+* Reload Gmail and start composing an email. After typing a few words you should see "< 1 min read" below the Send button.
+
+## Development / Modification
+
+Currently the calculation is based on reading at 180 words per minute. You can change this in `content.js`;
+
+* Clone repo.
+* Open [`content.js`](content.js).
+* Change `WORDS_PER_MINUTE = 180` to whatever you want.
+* Zip the folder (or use "Load unpacked extension") and re-install the extension.
+
---
+++ content.js
@@ -0,0 +1,69 @@
+InboxSDK.load('1', 'sdk_reading-time_cfb63eef93').then(function(sdk){
+
+    const WORDS_PER_MINUTE = 180;
+
+	// the SDK has been loaded, now do something with it!
+	sdk.Compose.registerComposeViewHandler(function(composeView){
+
+		const statusBarView = composeView.addStatusBar({});
+		statusBarView.el.classList.add('hb');
+		statusBarView.el.style.cssText = "font-size: 0.7em";
+
+		const timer = setInterval(function () {
+			const content = composeView.getTextContent();
+			if (!content) return;
+
+			const stats = readingTime(content, {wordsPerMinute: WORDS_PER_MINUTE});
+			let text = stats.text;
+			if (stats.time < 30000) {
+				text = `< 1 min read`;
+			}
+			statusBarView.el.innerHTML = text;
+		}, 1000);
+	});
+
+	// readingTime function taken from https://github.com/ngryman/reading-time/blob/master/lib/reading-time.js
+    function readingTime(text, options) {
+        var words = 0, start = 0, end = text.length - 1, wordBound, i
+
+        options = options || {}
+
+        // use default values if necessary
+        options.wordsPerMinute = options.wordsPerMinute || 200
+
+        // use provided function if available
+        wordBound = options.wordBound || ansiWordBound
+
+        // fetch bounds
+        while (wordBound(text[start])) start++
+        while (wordBound(text[end])) end--
+
+        // calculate the number of words
+        for (i = start; i <= end;) {
+            for (; i <= end && !wordBound(text[i]); i++) ;
+            words++
+            for (; i <= end && wordBound(text[i]); i++) ;
+        }
+
+        // reading time stats
+        var minutes = words / options.wordsPerMinute
+        var time = minutes * 60 * 1000
+        var displayed = Math.ceil(minutes.toFixed(2))
+
+        return {
+            text: displayed + ' min read',
+            minutes: minutes,
+            time: time,
+            words: words
+        }
+    }
+
+    function ansiWordBound(c) {
+        return (
+            (' ' === c) ||
+            ('\n' === c) ||
+            ('\r' === c) ||
+            ('\t' === c)
+        )
+    }
+});
